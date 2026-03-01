7739041887a1e63875d3db82f67889539346213f
nathabonfim59 <dev@nathabonfim59.com>
2026-03-01 13:35:47 -0300

    feat(embed): add embedded templates for config and systemd service

---
+++ embed/embed.go
@@ -0,0 +1,20 @@
+// Package embed provides embedded template files for hubbiott.
+// These templates are used for bootstrapping configuration and systemd services.
+package embed
+
+import "embed"
+
+// FS contains embedded template files.
+//
+//go:embed config.yaml.tmpl hubbiott.service.tmpl
+var FS embed.FS
+
+// ConfigTemplate is the raw content of the config.yaml.tmpl file.
+//
+//go:embed config.yaml.tmpl
+var ConfigTemplate string
+
+// SystemdServiceTemplate is the raw content of the hubbiott.service.tmpl file.
+//
+//go:embed hubbiott.service.tmpl
+var SystemdServiceTemplate string
